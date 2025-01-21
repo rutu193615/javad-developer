d1c13226677b09b59a8a683d18b055d82ae62767
rutu1936 <rutuagade15@gmail.com>
2025-01-21 16:48:14 +0530

    by rutu

---
+++ day1/src/settergetter/Student.java
@@ -0,0 +1,26 @@
+package settergetter;
+
+public class Student {
+int rollno;
+String name;
+double salary;
+public int getRollno() {
+	return rollno;
+}
+public void setRollno(int rollno) {
+	this.rollno = rollno;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public double getSalary() {
+	return salary;
+}
+public void setSalary(double salary) {
+	this.salary = salary;
+}
+
+}
---
+++ day1/src/settergetter/Test.java
@@ -0,0 +1,18 @@
+package settergetter;
+
+
+public class Test {
+public static void main(String[] args) {
+	
+	
+	
+	Student s=new Student();
+	s.setName("rutu");
+	s.setRollno(11);
+	s.setSalary(11.22);
+	System.out.println(s.getName());
+	System.out.println(s.rollno);
+	System.out.println(s.getSalary());
+	
+}
+}
